353e0abe988bafbb03820e1b7bb08b4ca8376f46
Vladimir HIl <vladimir.hil@lightmaker.com>
2015-12-20 23:57:16 +0100

    extend base SitecoreItem class

--- FortisDemo.Model/ISitecoreItem.cs
+++ FortisDemo.Model/ISitecoreItem.cs
@@ -1,6 +1,14 @@
-﻿namespace FortisDemo.Model
+﻿using Fortis.Model.Fields;
+using Sitecore.ContentSearch;
+
+namespace FortisDemo.Model
 {
 	public partial interface ISitecoreItem
 	{
+		[IndexField("__smallupdateddate")]
+		IDateTimeFieldWrapper UpdatedDate { get; }
+
+		[IndexField("__smallcreateddate")]
+		IDateTimeFieldWrapper CreatedDate { get; }
 	}
 }
--- FortisDemo.Model/SitecoreItem.cs
+++ FortisDemo.Model/SitecoreItem.cs
@@ -1,6 +1,50 @@
-﻿namespace FortisDemo.Model
+﻿using Fortis.Model.Fields;
+using Sitecore.Data;
+
+namespace FortisDemo.Model
 {
 	public partial class SitecoreItem : ISitecoreItem
 	{
+		public virtual IDateTimeFieldWrapper UpdatedDate
+		{
+			get { return GetField<DateTimeFieldWrapper>("__Updated", "__smallupdateddate"); }
+		}
+
+		public virtual IDateTimeFieldWrapper CreatedDate
+		{
+			get { return GetField<DateTimeFieldWrapper>("__Created", "__smallcreateddate"); }
+		}
+
+		public override string ToString()
+		{
+			return string.Format(
+				"{0} ({1}#{2}), id: {3}",
+				this.Name,
+				this.LanguageName,
+				this.DatabaseName,
+				new ID(ItemID));
+		}
+
+		public override bool Equals(object obj)
+		{
+			var p = obj as ISitecoreItem;
+
+			return this.Equals(p);
+		}
+
+		public bool Equals(ISitecoreItem p)
+		{
+			if (p == null)
+			{
+				return false;
+			}
+
+			return this.ItemID == p.ItemID;
+		}
+
+		public override int GetHashCode()
+		{
+			return ItemID.GetHashCode();
+		}
 	}
 }
